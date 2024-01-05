a6d3c92819fd7118121cc6d3286a2fbcc3f0261d
Caio Pascoal <caiocovreoficial@gmail.com>
2024-01-04 22:48:42 -0300

    Some more code in the 'BankingProgram' project

--- C++/projects/7_BankingProgram/BankingProgram.cpp
+++ C++/projects/7_BankingProgram/BankingProgram.cpp
@@ -1,34 +1,46 @@
 #include <iostream>
 #include <cmath>
 
-double VerifyNumberInput(double Num, std::string Frase) {
-    do {
-        std::cout << Frase;
-        std::cin >> Num;
-        if(Num){
-            return Num;
-        } else {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            std::cout << "Invalid input. Try again!\n";
-        }
-    } while(true);
+double ValidInput(bool Verified) {
+    if(Verified){
+        return true;
+    } else {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        std::cout << "Invalid input. Try again!";
+        return false;
+    }
 }
 
 double DepositMoney() {
-    double Money = 0;
-    Money = VerifyNumberInput(Money,"How many do you want o deposit? ");
+    double Money;
+    do {
+        std::cout << '\n' << "Enter the amount to deposity";
+        std::cout << '\n' << "-> ";
+        std::cin >> Money;
+    } while (!ValidInput(Money && Money <= 4));
     return Money;
 }
 
 int BankMenu() {
     int Choice;
-    Choice = VerifyNumberInput(Choice,"a");
+    do {
+        std::cout << '\n' << "********************";
+        std::cout << '\n' << "      BANK MENU";
+        std::cout << '\n' << "*******************";
+        std::cout << '\n' << "1 -> Show Balance";
+        std::cout << '\n' << "2 -> Deposit Money";
+        std::cout << '\n' << "3 -> Withdraw Money";
+        std::cout << '\n' << "4 -> Exit";
+        std::cout << '\n' << "-> ";
+        std::cin >> Choice;
+    } while (!ValidInput(Choice));
     return Choice;
 }
 
 int main() {
-    double CurrentAcount = 0;
-    CurrentAcount += BankMenu();
-    std::cout << CurrentAcount;
+    double CurrentAmount;
+    int MenuChoice;
+    CurrentAmount += DepositMoney();
+    std::cout << CurrentAmount;
 }
