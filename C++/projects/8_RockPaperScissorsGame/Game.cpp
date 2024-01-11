2fbb9d49320a555c920982cce04627fc22470adc
Caio Pascoal <caiocovreoficial@gmail.com>
2024-01-10 21:34:37 -0300

    Rock Papers Scissors Game Finished

--- C++/projects/8_RockPaperScissorsGame/Game.cpp
+++ C++/projects/8_RockPaperScissorsGame/Game.cpp
@@ -3,51 +3,89 @@
 #include <algorithm>
 #include <ctime>
 
+bool InvalidInpud(bool Input) {
+    if(Input) {
+        return false;
+    } else {
+        std::cout << '\n' << "Invalid Input."; 
+        std::cout << '\n' <<"Try again!";
+        return true;
+    }
+}
+
 char RockPaperScissors() {
     srand(time(NULL));
     int Choice = rand()%3+1;
-    char TextChoice;
     switch(Choice){
-        case 1: TextChoice = 'r'; break;
-        case 2: TextChoice = 'p'; break;
-        case 3: TextChoice = 's'; break;
+        case 1: return 'R';
+        case 2: return 'P';
+        case 3: return 'S';
     }
-    return TextChoice;
 }
 
-std::string GameResult(char ComputerMove, char PlayerMove){
+std::string TextRockPaperScissors(char Choice) {
+    switch(Choice){
+        case 'R': return "🪨";
+        case 'P': return "🧻";
+        case 'S': return "✂️";
+    }
+}
+
+std::string GameResult(char PlayerMove, char ComputerMove){
+    std::string WhoWins;
+    std::string PlayerWins = "Player Wins!";
+    std::string ComputerWins = "Computer Wins!";
+    std::string Tie = "Tie";
+    std::cout << '\n' << "You -> " << TextRockPaperScissors(PlayerMove);
+    std::cout << '\n' << "Computer -> " << TextRockPaperScissors(ComputerMove) << '\n';
     switch(ComputerMove == PlayerMove){
-        case true: std::cout << "Tie"; break;
+        case true: return Tie;
         case false:
             switch(ComputerMove){
-                case 'r': 
+                case 'R': 
                     switch(PlayerMove){
-                        case 'p': std::cout << "You Win"; break;
-                        case 's': std::cout << "You Lose"; break;
+                        case 'P': return PlayerWins;
+                        case 'S': return ComputerWins;
                     }
-                case 'p':
+                case 'P':
                     switch(PlayerMove){
-                        case 's': std::cout << "You Win"; break;
-                        case 'r': std::cout << "You Lose"; break;
+                        case 'S': return PlayerWins;
+                        case 'R': return ComputerWins;
                     }
-                case 's':
+                case 'S':
                     switch(PlayerMove){
-                        case 'r': std::cout << "You Win"; break;
-                        case 'p': std::cout << "You Lose"; break;
+                        case 'R': return PlayerWins;
+                        case 'P': return ComputerWins;
                     }
             }
     }
-    return 0;
 }
 
 int main() {
-    char Player;
-    char Computer;
-    std::cout << '\n' <<"Rock (r) Paper (p) Scissors (s)";
-    std::cout << '\n' <<"-> ";
-    // std::cin >> Player;
-    Player = 'r';
-    Computer = RockPaperScissors();
-    GameResult(Player, Computer);
+    bool Continue = true;
+    do {
+        char Player;
+        char Computer;
+        char Command;
+        std::string ValidCommand = "YN";
+        std::string ValidInput = "RPS";
+        do{
+            std::cout << '\n' <<"************************";
+            std::cout << '\n' <<"🪨  (R) 🧻 (P) ✂️  (S)";
+            std::cout << '\n' <<"************************";
+            std::cout << '\n' <<"-> ";
+            std::cin >> Player;
+            Player = toupper(Player);
+        } while (InvalidInpud(Player && (ValidInput.find(Player) != std::string::npos)));
+        Computer = RockPaperScissors();
+        std::cout << GameResult(Player, Computer);
+        do {
+            std::cout << '\n' << "Continue? [Y] [N]";
+            std::cout << '\n' << "-> ";
+            std::cin >> Command;
+            Command = toupper(Command);
+        } while(InvalidInpud(Command && (ValidCommand.find(Command) != std::string::npos)));
+        Command == 'N' ? Continue = false : Continue;
+    } while (Continue);
     return 0;
 }
